62a035a8926d6c4b10620cd5703e7dd33a9bb193
Boris Kayi <boriskayienzo@gmail.com>
2020-05-23 01:51:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GitRepo
+A cli package that automates adding a github remote to your local repository
--- gitrepo.py
+++ gitrepo.py
@@ -2,4 +2,4 @@ import click
 
 @click.command()
 def create():
-    click.echo("Hello Word")
+    click.echo("Hello Word 🔥️")
