28f35067fb9529a6ad192acf9965a620355c1a0a
Boris Kayi <boriskayienzo@gmail.com>
2020-05-24 01:51:39 +0200

    Edit module function

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
 
+version='1.0'
 setup(
     name='Git-Repo',
-    version='1.0',
+    version=version,
     author='Boris Kayi & Herve Musangwa',
     description='A CLI app to create a new git repository both locally and remotely',
     install_requires=[
@@ -11,6 +12,6 @@ setup(
     ],
     entry_points='''
     [console_scripts]
-    gitrepo=gitrepo:create
+    gitrepo=gitrepo:start
     '''
 )
