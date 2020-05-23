e708ecdb06d12c37dae31624e495a1a9cb0cccc1
Boris Kayi <boriskayienzo@gmail.com>
2020-05-24 01:51:24 +0200

    Add creating repo and get url

--- gitrepo.py
+++ gitrepo.py
@@ -1,5 +1,32 @@
 import click
+from github import Github
+from github.GithubException import BadCredentialsException
+
 
 @click.command()
-def create():
-    click.echo("Hello Herve, It's working bro 🔥️🔥️🔥️")
+def start():
+    username = click.prompt('Username')
+    password = click.prompt('Password', hide_input=True)
+    user = Github(username, password)
+    name = click.prompt('Repo name: ')
+    is_public = click.prompt('Should the repository be public? (y/N)\n')
+
+    if is_public.lower() == 'y' or is_public.lower() == 'yes':
+        is_public = True
+    elif is_public.lower() == 'n' or is_public.lower() == 'no':
+        is_public = False
+    else:
+        is_public = True
+
+    try:
+        user.get_user().create_repo(  # Creating a repository on github
+            name=name,
+            private=not is_public,
+            auto_init=True,
+        )
+
+        repo_url = user.get_user().get_repo(name).clone_url
+        print(f'Repository created on github, here is the url: {repo_url}')
+
+    except BadCredentialsException:
+        print('Wrong username or password')
