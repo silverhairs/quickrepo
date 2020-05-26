6bf22c88dfa23e9e983d7f7d98e5769540cc2d2d
Boris Kayi <boriskayienzo@gmail.com>
2020-05-26 04:24:35 +0200

    Code cleaning

--- gitrepo.py
+++ gitrepo.py
@@ -1,32 +1,67 @@
 import click
 import os
+import sys
 import git
-from github import Github, GithubException, Badusername_holderException, BadCredentialsException
+from github import Github
 
 
-# This function generates a txt file that holds username
+# This function generates a txt file that holds the username
 def generate_username_file(file_name, username):
     # Hide file in (Unix systems)
     prefix = '.' if os.name != 'nt' else ''
     file_name = f'{prefix}{file_name}'
-
     # Write username inside file
     with open(file_name, 'w') as f:  # Create  file
         f.write(f'{username}')
         f.close()
-    # File Key
 
+
+# Create repository on github.com and clone it locally
+def create_and_clone_repo(user):
+    repo_name = click.prompt('Repo name: ')
+    public = click.prompt('Should the repository be public?\n[y/N]: ')
+
+    count = 1
+    while count > 0:
+        if public.lower() == 'y' or public.lower() == 'yes':
+            public = True
+            count -=1
+        elif public.lower() == 'n' or public.lower() == 'no':
+            public = False
+            count-=1
+        else:
+            click.echo('Invalid input!')
+            public = click.prompt('Should the repository be public?\n[y/N]: ')
+    
+    desc = click.prompt('Description: ')
+
+
+    try:
+        click.echo(f'Creating repository {repo_name} ...')
+
+        user.create_repo(  
+            name=repo_name,
+            description=desc,
+            private=not public,
+            auto_init=True,
+        )
+        repo_url = user.get_repo(repo_name).clone_url
+
+        git.Git(os.getcwd()).clone(repo_url)
+        click.secho('Repository successfully created! 🔥️🔥️', fg='green')
+
+    except Exception as e:
+        click.secho(f'{repr(e)}', fg='red')
 
 
 @click.command()
-def start():
+def main():
     connection = None
 
-    # If file exists, fetch username and connect, else request all the username_holder and generate file
+    # If the username file exists, use it, else requests credentials and generate the file
     if os.path.exists('.username_holder.txt'):
         password = click.prompt('Enter GitHub password: ', hide_input=True)
         with open('.username_holder.txt') as f:
-            cred = {}
             for line in f:
                 username = line
             connection = Github(username, password)
@@ -36,47 +71,12 @@ def start():
         username = click.prompt('Username')
         password = click.prompt('Password', hide_input=True)
         connection = Github(username, password)
-
-        generate_username_file('username_holder.txt', username)
-
-    name = click.prompt('Repo name: ')
-    is_public = click.prompt('Should the repository be public?\n[y/N]: ')
-    
-    # If user enter y or yes, repo is public, user enters n or no, repo is private else, reask question until users gives valid answer
-    count = 1
-    while count > 0:
-        if is_public.lower() == 'y' or is_public.lower() == 'yes':
-            is_public = True
-            count -=1
-        elif is_public.lower() == 'n' or is_public.lower() == 'no':
-            is_public = False
-            count-=1
-        else:
-            click.echo('Invalid input!')
-            is_public = click.prompt('Should the repository be public?\n[y/N]: ')
-
-
     try:
-        # Create a repo on github
-        connection.get_user().create_repo(  
-            name=name,
-            private=not is_public,
-            auto_init=True,
-        )
-        # Clone the repo locally
-        repo_url = connection.get_user().get_repo(name).clone_url
-        git.Git(os.getcwd()).clone(repo_url)
+        user = connection.get_user() #FIXME: request not being made
+        click.secho('User authenticated\n', fg='green')
+    except Exception as e:
+        click.secho(f'{repr(e)}', fg='red')
+        sys.exit()
 
-        click.echo(click.style('Repository successfully created! 🔥️🔥️', fg='green'))
-
-    except GithubException as error:
-        click.echo(
-            click.style(str(error.data['errors'][0]['message']).capitalize(),
-                        fg='red'))
-
-    except Badusername_holderException as error:
-        click.echo(
-            click.style(str(error['message']).capitalize() + ' ❌❌', fg='red'))
-        
-    except BadCredentialsException:
-        click.echo(click.style('Wrong username or password'), fg='red')
+    generate_username_file('username_holder.txt', username)
+    create_and_clone_repo(user)
--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(
     ],
     entry_points='''
     [console_scripts]
-    gitrepo=gitrepo:start
+    gitrepo=gitrepo:main
     '''
 )
