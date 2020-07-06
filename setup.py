4730a227cec5cc64053ecb0200bcba8f15b57460
Boris Kayi <boriskayienzo@gmail.com>
2020-07-06 21:31:21 +0200

    Added testing dependencies

--- setup.py
+++ setup.py
@@ -1,39 +1,39 @@
 from setuptools import setup
 import os
 
-version = '1.0.1'
+version = "1.0.3"
 current = os.path.abspath(os.path.dirname(__file__))
 # Getting long description
-with open(os.path.join(current, 'README.md'), 'r') as f:
+with open(os.path.join(current, "README.md"), "r") as f:
     readme = f.read()
 
 setup(
-    name='quickrepo',
+    name="quickrepo",
     version=version,
-    url='https://www.github.com/silverhairs/quickrepo',
-    author='Boris Kayi',
-    author_email='boriskayienzo@gmail.com',
-    description=
-    'Command-line tool to automate git and github repository creation.',
+    url="https://www.github.com/silverhairs/quickrepo",
+    author="Boris Kayi",
+    author_email="boriskayienzo@gmail.com",
+    description="Command-line tool to automate git and github repository creation.",
     long_description=readme,
-    long_description_content_type='text/markdown',
-    platforms='any',
-    python_requires='>=3.6',
-    packages=['quickrepo'],
-    py_modules=['quickrepo'],
+    long_description_content_type="text/markdown",
+    platforms="any",
+    python_requires=">=3.6",
+    packages=["quickrepo"],
+    py_modules=["quickrepo"],
     install_requires=[
-        'Click',
-        'PyGithub',
-        'gitpython',
-        'colorama',
+        "click==7.1.2",
+        "PyGithub==1.51",
+        "GitPython==3.1.3",
+        "colorama==0.4.3",
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    entry_points='''
+    extras_require={"test": ["pytest==5.4.3",]},
+    entry_points="""
     [console_scripts]
-    quickrepo=quickrepo:cli
-    ''',
+    quickrepo=quickrepo:main
+    """,
 )
